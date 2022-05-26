d6351e691867e73151671ad71f37d93aec3887c5
Linn Johansson <epostlinn@outlook.com>
2022-05-26 18:01:14 +0200

    first commit

--- RestAPI_Mashup/Models/AddArtistRequest.cs
+++ RestAPI_Mashup/Models/AddArtistRequest.cs
@@ -2,7 +2,7 @@
 {
     public class AddArtistRequest
     {
-        //för att kunna lägga in egna artister i databasen
+        //för att kunna lägga in egna artister i databasen 
         public string mbid { get; set; }
         public string description { get; set; }
         public List<Album> albums { get; set; } = new List<Album>();
